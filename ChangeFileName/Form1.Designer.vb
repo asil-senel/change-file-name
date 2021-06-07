f63aa6e017dd8375066e10b0637c594b798d0b37
asil-senel <84016299+asil-senel@users.noreply.github.com>
2021-06-07 10:40:40 +0300

    Add files via upload

---
+++ ChangeFileName/Form1.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(566, 389)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 60)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Change File Name"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(70, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Location"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(160, 50)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(170, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(160, 120)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(170, 20)
+        Me.TextBox3.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(449, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Target Name"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(539, 120)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(170, 20)
+        Me.TextBox4.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(70, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Source Name"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(160, 241)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(170, 20)
+        Me.TextBox5.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(449, 243)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(84, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Target File Type"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(539, 240)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(170, 20)
+        Me.TextBox6.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(70, 244)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(87, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Source File Type"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(184, 389)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(110, 60)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(184, 189)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(35, 20)
+        Me.TextBox7.TabIndex = 2
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(94, 192)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Range"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(243, 192)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(16, 13)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "to"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(289, 189)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(35, 20)
+        Me.TextBox8.TabIndex = 3
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(555, 185)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(35, 20)
+        Me.TextBox9.TabIndex = 7
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(611, 188)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(16, 13)
+        Me.Label9.TabIndex = 20
+        Me.Label9.Text = "to"
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(657, 185)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(35, 20)
+        Me.TextBox10.TabIndex = 8
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(462, 188)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(39, 13)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Range"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(184, 324)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(85, 17)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "Parentheses"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(566, 324)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(85, 17)
+        Me.CheckBox2.TabIndex = 10
+        Me.CheckBox2.Text = "Parentheses"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(555, 37)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(126, 44)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "Clear Address"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 461)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Change File Name"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Button3 As Button
+End Class
---
+++ ChangeFileName/Form1.vb
@@ -0,0 +1,113 @@
+﻿Public Class Form1
+    Dim sSourceLoc As String
+    Dim sSourceName As String
+    Dim sSourceType As String
+
+    Dim sTargetName As String
+    Dim sTargetType As String
+
+    Dim iDiffUp As Integer
+    Dim iDiffDown As Integer
+    Dim iDiff As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sSourceLoc = TextBox1.Text
+        sSourceName = TextBox3.Text
+        sSourceType = TextBox5.Text
+
+        If TextBox4.Text = "" Then
+            sTargetName = TextBox3.Text
+        Else
+            sTargetName = TextBox4.Text
+        End If
+
+        If TextBox6.Text = "" Then
+            sTargetType = TextBox5.Text
+        Else
+            sTargetType = TextBox6.Text
+        End If
+
+        If ((TextBox7.Text <> "") Or (TextBox8.Text <> "") Or (TextBox9.Text <> "") Or (TextBox10.Text <> "")) Then
+            iDiffUp = CInt(TextBox10.Text) - CInt(TextBox8.Text)
+            iDiffDown = CInt(TextBox9.Text) - CInt(TextBox7.Text)
+        End If
+
+        If iDiffUp = iDiffDown Then
+            iDiff = iDiffDown
+        End If
+
+        If ((TextBox7.Text <> "") Or (TextBox8.Text <> "")) Then 'index on for both
+            If ((CheckBox1.Checked) = True And (CheckBox2.Checked = False)) Then 'source parentheses exist, target parentheses don't exist
+                For rangeS As Integer = TextBox7.Text To TextBox8.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + "(" + CStr(rangeS) + ")" + "." + sSourceType, sTargetName + CStr(rangeS + iDiff) + "." + sTargetType)
+                Next
+            ElseIf ((CheckBox1.Checked) = True And (CheckBox2.Checked = True)) Then 'both source and target parentheses exist
+                For rangeS As Integer = TextBox7.Text To TextBox8.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + "(" + CStr(rangeS) + ")" + "." + sSourceType, sTargetName + "(" + CStr(rangeS + iDiff) + ")" + "." + sTargetType)
+                Next
+            ElseIf ((CheckBox1.Checked) = False And (CheckBox2.Checked = True)) Then 'source parentheses don't exist, target parentheses do
+                For rangeS As Integer = TextBox7.Text To TextBox8.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + CStr(rangeS) + "." + sSourceType, sTargetName + "(" + CStr(rangeS + iDiff) + ")" + "." + sTargetType)
+                Next
+            Else 'both source and target parentheses don't exist
+                For rangeS As Integer = TextBox7.Text To TextBox8.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + CStr(rangeS) + "." + sSourceType, sTargetName + CStr(rangeS + iDiff) + "." + sTargetType)
+                Next
+            End If
+        ElseIf ((TextBox7.Text <> "") And (TextBox8.Text <> "") And (TextBox9.Text = "") And (TextBox10.Text = "")) Then 'index on for source
+            If ((CheckBox1.Checked) = True And (CheckBox2.Checked = False)) Then 'source parentheses exist, target parentheses don't exist
+                For rangeS As Integer = TextBox7.Text To TextBox8.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + "(" + CStr(rangeS) + ")" + "." + sSourceType, sTargetName + "." + sTargetType)
+                Next
+            ElseIf ((CheckBox1.Checked) = True And (CheckBox2.Checked = True)) Then 'both source and target parentheses exist
+                For rangeS As Integer = TextBox7.Text To TextBox8.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + "(" + CStr(rangeS) + ")" + "." + sSourceType, sTargetName + "." + sTargetType)
+                Next
+            ElseIf ((CheckBox1.Checked) = False And (CheckBox2.Checked = True)) Then 'source parentheses don't exist, target parentheses do
+                For rangeS As Integer = TextBox7.Text To TextBox8.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + CStr(rangeS) + "." + sSourceType, sTargetName + "." + sTargetType)
+                Next
+            Else 'both source and target parentheses don't exist
+                For rangeS As Integer = TextBox7.Text To TextBox8.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + CStr(rangeS) + "." + sSourceType, sTargetName + CStr(rangeS + iDiff) + "." + sTargetType)
+                Next
+            End If
+        ElseIf ((TextBox7.Text = "") And (TextBox8.Text = "") And (TextBox9.Text <> "") And (TextBox10.Text <> "")) Then 'index on for target
+            If ((CheckBox1.Checked) = True And (CheckBox2.Checked = False)) Then 'source parentheses exist, target parentheses don't exist
+                For rangeS As Integer = TextBox9.Text To TextBox10.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + "." + sSourceType, sTargetName + CStr(rangeS + iDiff) + "." + sTargetType)
+                Next
+            ElseIf ((CheckBox1.Checked) = True And (CheckBox2.Checked = True)) Then 'both source and target parentheses exist
+                For rangeS As Integer = TextBox9.Text To TextBox10.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + "." + sSourceType, sTargetName + "(" + CStr(rangeS + iDiff) + ")" + "." + sTargetType)
+                Next
+            ElseIf ((CheckBox1.Checked) = False And (CheckBox2.Checked = True)) Then 'source parentheses don't exist, target parentheses do
+                For rangeS As Integer = TextBox9.Text To TextBox10.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + "." + sSourceType, sTargetName + "(" + CStr(rangeS + iDiff) + ")" + "." + sTargetType)
+                Next
+            Else 'both source and target parentheses don't exist
+                For rangeS As Integer = TextBox9.Text To TextBox10.Text
+                    My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + "." + sSourceType, sTargetName + CStr(rangeS + iDiff) + "." + sTargetType)
+                Next
+            End If
+        ElseIf ((TextBox7.Text = "") And (TextBox8.Text = "") And (TextBox9.Text = "") And (TextBox10.Text = "")) Then 'index off for both
+            My.Computer.FileSystem.RenameFile(sSourceLoc + sSourceName + "." + sSourceType, sTargetName + "." + sTargetType)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+        TextBox9.Text = ""
+        TextBox10.Text = ""
+        CheckBox1.CheckState = CheckState.Unchecked
+        CheckBox2.CheckState = CheckState.Unchecked
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = ""
+    End Sub
+End Class
